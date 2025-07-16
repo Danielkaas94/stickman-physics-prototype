ac461f4d25d13713ae24a3b7bc2de09328bc19cc
Daniel Kaas Bundgaard Jørgensen <danielkj94@hotmail.com>
2025-07-16 11:22:28 +0200

    A bit of cleaning ✨🧹✨

--- experiments/LegacyFile_First_ProofOfConcept.py
+++ experiments/LegacyFile_First_ProofOfConcept.py
@@ -1,5 +1,3 @@
-print("JUST TESTING")
-
 import pygame
 import pymunk
 import pymunk.pygame_util
@@ -23,40 +21,13 @@ mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 dragged_joint = None
 dragged_shape = None
 
-
 # Create a static floor
 floor = pymunk.Segment(space.static_body, (0, 580), (800, 580), 5)
 floor.elasticity = 0.5
-# floor.friction = 1.0 # WTF?
-# floor.friction = 0.1 # WTF?
+# floor.friction = 1.0 # WTF friction not working?
 space.add(floor)
 
-
-# Stickman body (a simple circle for head, segments for limbs)
-def create_stickman_old(space, x, y):
-    # Head
-    head_body = pymunk.Body(1, pymunk.moment_for_circle(1, 0, 15))
-    head_body.position = x, y
-    head_shape = pymunk.Circle(head_body, 15)
-    head_shape.elasticity = 0.5
-    space.add(head_body, head_shape)
-
-    # Torso
-    torso_body = pymunk.Body(2, pymunk.moment_for_segment(2, (0, 0), (0, 50), 5))
-    torso_body.position = x, y + 30
-    torso_shape = pymunk.Segment(torso_body, (0, 0), (0, 50), 5)
-    torso_shape.elasticity = 0.5
-    space.add(torso_body, torso_shape)
-
-    # Neck joint
-    joint = pymunk.PinJoint(head_body, torso_body, (0, 15), (0, 0))
-    space.add(joint)
-
-    return [head_body, torso_body]
-
-# stickman = create_stickman_old(space, 400, 100)
-
-def create_stickman(space, x, y):
+def create_stickman(space, x, y, color=(0,0,0)):
     bodies = []
 
     # Head
@@ -102,36 +73,50 @@ def create_stickman(space, x, y):
     # return bodies # Old return
     return {
         "bodies": bodies,
-        "torso": torso_body
-    }
+        "torso": torso_body,
+        "player_color": color
+    }, color
+
+stickmen = []
+stickman1, color1 = create_stickman(space, 123, 100,(0,0,255))
+stickmen.append((stickman1, color1))
+
+stickman2, color2 = create_stickman(space, 700, 124,(0,255,00))
+stickmen.append((stickman2, color2))
+
+BadGuy_stickman_NPC1, color3 = create_stickman(space, 400, 123,(255,0,0))
+BadGuy_stickman_NPC2, color4 = create_stickman(space, 123, 400,(255,0,0))
+stickmen.append((BadGuy_stickman_NPC1, color3))
+stickmen.append((BadGuy_stickman_NPC2, color4))
 
 
-stickman = create_stickman(space, 123, 100)
-stickman = create_stickman(space, 400, 123)
-stickman = create_stickman(space, 345, 100)
-stickman = create_stickman(space, 400, 345)
-stickman = create_stickman(space, 234, 100)
-stickman2 = create_stickman(space, 700, 124)
+screen.fill((255, 255, 255))  # Clear screen (white background)
 
-# What?
-# print(Fore.RED + stickman["torso"]) # not working
-# print(Fore.RED + f"stickman: {stickman["torso"]}")
-# stickman["torso"]
+print("HEY!!!!!!!")
+# Use Color
+for shapes, color in stickmen:
+    print("HELLO!#!#?") 
+    for shape in shapes:
+        if isinstance(shape, pymunk.Segment):
+            body = shape.body
+            a = body.position + shape.a.rotated(body.angle)
+            b = body.position + shape.b.rotated(body.angle)
+            print(color)
+            print("HELLO?") # I don't get inside this part?
+            pygame.draw.line(screen, color, a, b, int(shape.radius * 2))
+        elif isinstance(shape, pymunk.Circle):
+            pos = int(shape.body.position.x), int(shape.body.position.y)
+            pygame.draw.circle(screen, color, pos, int(shape.radius))
+
+
 # help(torso.apply_impulse_at_local_point) # See if there's a docstring
-
-
-# Old event
-'''    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-'''
 
 running = True
 # This is my main event loop, correct?
 while running:
     screen.fill((30, 30, 30))
 
-    upright = pymunk.DampedRotarySpring(space.static_body, stickman["torso"], 0, 2000000, 100000)
+    upright = pymunk.DampedRotarySpring(space.static_body, stickman1["torso"], 0, 2000000, 100000)
     space.add(upright)
 
 
@@ -141,11 +126,11 @@ while running:
         mouse_point = pymunk.pygame_util.from_pygame(mouse_pos, screen)
         mouse_body.position = mouse_point
 
-    # Keypress - Space logic
+    # Keypress - Space logic - Jump
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        torso = stickman["torso"]
-        torso.apply_impulse_at_local_point((0,-500)) # Jump
+        torso = stickman1["torso"]
+        torso.apply_impulse_at_local_point((0,-500))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -172,11 +157,6 @@ while running:
     space.debug_draw(draw_options)
     pygame.display.flip()
     clock.tick(70) # Frames/tick
-
-
-
-
-
 
 print("Winners never quit! Quitters never win!")
 pygame.quit()
